aec679895eea2d940104c476e5b1841f356a69f4
Mikhail Nikulin <nklnmish@gmail.com>
2025-01-19 21:40:33 -0500

    9:30 lesson 10

--- client/src/components/disk/fileList/FileList.jsx
+++ client/src/components/disk/fileList/FileList.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
 import "./styles/fileList.css"
-import {useSelector} from "react-redux"
+import { useSelector } from "react-redux"
 import File from "./file/File"
 
 const FileList = () => {
 
-    // const files = useSelector(state => state.files.files).map( file => <File/>)
-    const files = [{_id:1, name: "direc", type: 'dir',size:"5gb", date:"1.19.2025"}].map(file => <File key={file.id}/>)
-    
+  // const files = useSelector(state => state.files.files).map( file => <File/>)
+  const files = [
+    { _id: 1, name: "direc", type: 'dir', size: "5gb", date: "1.19.2025", key: "1"},
+    { _id: 2, name: "direc2", type: 'dir', size: "2gb", date: "1.19.2025", key: "2" }
+  ].map(file => <File key={file.id} />)
+
 
   return (
     <div className="filelist">
-        <div className="filelist__header">
-            <div className="filelist__name">Name</div>
-            <div className="filelist__date">Date</div>
-            <div className="filelist__size">Size</div>
-        </div>
+      <div className="filelist__header">
+        <div className="filelist__name">Name</div>
+        <div className="filelist__date">Date</div>
+        <div className="filelist__size">Size</div>
+      </div>
+      {files}
     </div>
   )
 }
--- client/src/components/disk/fileList/file/File.jsx
+++ client/src/components/disk/fileList/file/File.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const File = () => {
+const File = (key) => {
   return (
     <div>File</div>
   )
