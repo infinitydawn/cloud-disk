7fe351590982db04cd6af2a86d579d7399b21b70
Mikhail Nikulin <Michaelnikulinusa@gmail.com>
2025-02-16 22:19:21 -0500

    finished 13

--- server/controllers/fileController.js
+++ server/controllers/fileController.js
@@ -1,6 +1,8 @@
 const fileService = require("../services/fileService")
 const User = require('../models/User')
 const File = require('../models/File')
+const config = require("config")
+const fs = require("fs")
 
 class FileController {
     async createDir(req, res){
@@ -34,6 +36,67 @@ class FileController {
         } catch (error) {
             console.log(error)
             return res.status(500).json({message: "cannot get files"})
+        }
+    }
+
+    async uploadFile(req,res){
+        try {
+            // the file to save
+            const file = req.files.file
+
+            // find parent folder and user in DB
+            const parent = await File.findOne({user: req.user.id, _id: req.body.parent})
+            const user = await User.findOne({_id: req.user.id})
+
+            // if not enough space return error
+            if (user.usedSpace + file.size > user.diskSpace){
+                return res.status(400).json({message: "There is no space on the disk"})
+            }
+
+            // update used space
+            user.usedSpace = user.usedSpace + file.size
+
+            let path;
+
+            // decide path if there is parent or not
+            if(parent) {
+                path = `${config.get('filePath')}\\${user._id}\\${parent.path}\\${file.name}`
+            } else {
+                path = `${config.get('filePath')}\\${user._id}\\${file.name}`
+            }
+
+            // check if this file already exists
+            if(fs.existsSync(path)){
+                return res.status(400).json({message: 'File already exists.'})
+            }
+
+            // save file by path
+            file.mv(path)
+
+            // get the file extension type
+            const type = file.name.split('.').pop()
+
+            // create model of the file
+            const dbFile = new File({
+                name: file.name,
+                type,
+                size: file.size,
+                path: parent?.path,
+                parent: parent?._id,
+                user: user._id
+            })
+
+            // save file and user in database
+            await dbFile.save()
+            await user.save()
+
+
+            // respond back to client
+            res.json(dbFile)
+
+        } catch (error) {
+            console.log(e)
+            return res.status(400).json({message: "Upload error"})
         }
     }
 }
--- server/index.js
+++ server/index.js
@@ -3,10 +3,12 @@ const mongoose = require("mongoose")
 const config = require("config")
 const authRouter = require("./routes/auth.routes")
 const fileRouter = require("./routes/file.routes.js")
+const fileUpload = require("express-fileupload")
 const app = express();
 const PORT = config.get("serverPort")
 const corsMiddleware = require("./middleware/cors.middleware.js")
 
+app.use(fileUpload({}))
 app.use(corsMiddleware)
 app.use(express.json())
 app.use("/api/auth",authRouter)
